602694d78a4c21758de654ce1b0b804198a9f604
czwjss <1659962583@qq.com>
2021-07-21 15:13:41 +0800

    once change

---
+++ hh.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-12 03:38:07
+ * @LastEditTime: 2021-07-21 14:55:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \hh\hh.cpp
+ */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<3;++i)
+        cout<<"czwjss"<<endl;
+    return 0;
+}
